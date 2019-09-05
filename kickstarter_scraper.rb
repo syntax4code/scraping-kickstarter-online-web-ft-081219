578fc15fb7984256fb9dc6a5898246d31832e68a
Warren Harper II <warrenharper2@att.net>
2019-09-05 23:41:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 require 'nokogiri'
 require 'pry'
 def create_project_hash
