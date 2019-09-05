99b01ff12d23628a570ee173f3394b181ba726c9
Warren Harper II <warrenharper2@att.net>
2019-09-05 23:47:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,14 +15,14 @@ end
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
- 
+
   projects = {}
- 
+
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
- 
+
   # return the projects hash
   projects
 end
